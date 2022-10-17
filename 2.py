99dd07f531ff441a7c8169df8344e3558ec53490
Code4ever10 <115994891+Code4ever10@users.noreply.github.com>
2022-10-17 17:33:42 +0530

    Update 2.py
    Comment

--- 2.py
+++ 2.py
@@ -4,7 +4,7 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
-image=cv.imread(r'C:\tmp\0000.png')
+image=cv.imread(r'C:\tmp\0000.png'). # file from your pc
 half=cv.resize(image,(0,0),fx=0.6 ,fy=1.0)#the values inside() decides the limits of x n y axis   
 bigger=cv.resize(image,(1050,1610))#fx defines the length whereas fy defines the breadth  of a any quadilateral
 stretch_near=cv.resize(image,(780,540),interpolation=cv.INTER_NEAREST)#interpolation is used to calculate the pixcels values
