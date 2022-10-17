2509d64fcc0db71a1bbdbed903e237d42d6c7ae5
Code4ever10 <115994891+Code4ever10@users.noreply.github.com>
2022-10-17 17:36:48 +0530

    Update 8.py
    Comment

--- 8.py
+++ 8.py
@@ -5,12 +5,12 @@ import cv2
 img = np.zeros((300,300,3))
 
 img[0:100,0:100]= 255,255,255
-img[100:200,100:200] = 255,255,255
+img[100:200,100:200] = 255,255,255 
 
 img[0:100,200:300] = 255,255,255
 img[200:300,0:100] = 255,255,255
 img[200:300,200:300] = 255,255,255
 
-cv2.imshow('CHECKER BOARD',img)
+cv2.imshow('CHECKER BOARD',img) #display output 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
